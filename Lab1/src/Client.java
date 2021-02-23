74751b1c6cc9cdcc9e54f2aaf193250ba1f46bbf
Ca-moes <andre3000pt@gmail.com>
2021-02-23 11:31:17 +0000

    Small input validation on Client

--- Lab1/src/Client.java
+++ Lab1/src/Client.java
@@ -4,6 +4,11 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        if (args.length != 5 && args.length != 4){
+            System.out.println("java Client <host> <port> <oper> <opnd>*");
+            return;
+        }
+
         System.out.println(args[0]); //host
         System.out.println(args[1]); //port
         System.out.println(args[2]); //oper   - "register" / "lookup"
@@ -11,8 +16,16 @@ public class Client {
         System.out.println(args[4]); //opnd2* - <IP address> for register
 
         DatagramSocket socket = new DatagramSocket(3001);
+        String command = "invalid";
 
-        String command = new String(args[2] + " " + args[3] + " " + args[4]);
+        if (args[2].equals("register")){
+            command = new String("register " + args[3] + " " + args[4]);
+        } else if (args[2].equals("lookup")){
+            command = new String("lookup " + args[3]);
+        } else
+            return;
+
+
         byte[] buf = command.getBytes();
         InetAddress address = InetAddress.getByName(args[0]);
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, Integer.parseInt(args[1]));
